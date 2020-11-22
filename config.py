4ae2b6490376561a7fb16c030e567bc3d8944aa5
mohamed209 <mohamedmosad209@gmail.com>
2020-11-23 01:19:54 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,98 @@
+# Absolute path to the application folder
+APPLICATION_PATH = '/opt/nvidia/deepstream/deepstream-5.0/sources/python/apps/deepstream-amtraffic'
+
+# Save detected bounding boxes (Vehicles) into a file located in application folder
+SAVE_BBOXES_EACHFRAME = False
+BBOXES_EACHFRAME_FILENAME = 'file_bboxesEachFrame.txt'
+
+# Save images of detected vehicles
+SAVE_IMAGES = False
+
+# Enable License Plate anonymization function
+ANONYMIZE_LICENSE_PLATES = True
+
+# Save detected license plate information into a file located in application folder
+SAVE_LICENSE_PLATES_TO_FILE = True
+LICENSE_PLATES_FILENAME = 'file_licensePlatesDetections.txt'
+
+# Save detected objects archive into a file located in application folder
+SAVE_ARCHIVE_TO_FILE = True
+ARCHIVE_FILENAME = 'file_archiveDetections.txt'
+
+# Save traffic statistics into a file located in application folder
+SAVE_STATISTICS_TO_FILE = True
+STATISTICS_FILENAME = 'file_statisticsDetections.txt'
+
+# When TRAFFICSTATS_FRAME_RATE triggers, save image with current traffic points (where small circles are points, green - not-clustered, blue - clustered)
+SAVE_IMAGES_WITH_EXIT_POINTS = False
+
+# Send IOT MQTT messages to AWS IoT service
+SEND_IOT_MESSAGES = False
+
+# Print debug information
+PRINT_DEBUG = False
+
+# Number of frames to run License Plate detection and recognition algorithms
+ALPR_FRAME_RATE = 5
+# Number of frames to calculate traffic statistics
+TRAFFICSTATS_FRAME_RATE = 3600
+# Minimum age of an object to consider it a proper detection
+MIN_DETECTION_AGE = 15  # 15 frames = 1 second
+# For how long object can be considered missing
+ABSENSE_INTERVAL = 15
+# Minimal number of LP samples to run license plate recognition
+MIN_NUMBER_OF_LP_SAMPLES = 6
+# Minimal confidence for license plate recognition record
+ALRP_CONFIDENCE_THRESHOLD = 70
+
+# Grouping algorithm settings
+DBSCAN_EPSILON = 0.5
+DBSCAN_SAMPLES = 10
+
+# CONFIG FILES FOR TrafficCamNet, CarTypeNet and MQTT module
+PGIE_CONFIG_FILE = "pgie_config.txt"
+SGIE_CONFIG_FILE = "sgie_config.txt"
+MSCONV_CONFIG_FILE = "dstest4_msgconv_config.txt"
+
+# Tracker config files and utilized tracker
+TRACKER_KLT_CONFIG = 'tracker_klt_config.txt'
+TRACKER_NVDCF_CONFIG = 'tracker_nvdcf_config.txt'
+TRACKER_IOU_CONFIG = 'tracker_iou_config.txt'
+TRACKER_CONFIG = TRACKER_KLT_CONFIG
+
+# Object detector (TrafficCamNet) classes
+PGIE_UNIQUE_ID = 1
+PGIE_CLASS_ID_VEHICLE = 0
+PGIE_CLASS_ID_BICYCLE = 1
+PGIE_CLASS_ID_PERSON = 2
+PGIE_CLASS_ID_ROADSIGN = 3
+
+PGIE_LABELS_DICT = {
+    -1: 'None',
+    0: 'Car',
+    1: 'TwoWheeler',
+    2: 'Person',
+    3: 'RoadSign',
+}
+
+# Secondary detector (Vehicle Type) classes
+SGIE_UNIQUE_ID = 2
+SGIE_CLASS_ID_COUPE = 0
+SGIE_CLASS_ID_LARGE = 1
+SGIE_CLASS_ID_SEDAN = 2
+SGIE_CLASS_ID_SUV = 3
+SGIE_CLASS_ID_TRUCK = 4
+SGIE_CLASS_ID_VAN = 5
+
+SGIE_LABELS_DICT = {
+    -1: 'None',
+    0: 'Coupe',
+    1: 'LargeVehicle',
+    2: 'Sedan',
+    3: 'SUV',
+    4: 'Truck',
+    5: 'Van',
+}
+
+# Object detector (FaceLicensePlates) classes
+PGIE2_UNIQUE_ID = 3
---
+++ main_script.py
@@ -0,0 +1,398 @@
+#!/usr/bin/env python3
+
+################################################################################
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+################################################################################
+
+from config import *
+import pyds
+from common.FPS import GETFPS
+from common.bus_call import bus_call
+from common.is_aarch_64 import is_aarch64
+from gi.repository import GObject, Gst
+import gi
+import configparser
+import platform
+import sys
+sys.path.append('../')
+
+
+gi.require_version('Gst', '1.0')
+
+OSD_PROCESS_MODE = 0
+fps_streams ={}
+def osd_sink_pad_buffer_probe(pad, info, u_data):
+    frame_number = 0
+    # Intiallizing object counter with 0.
+    obj_counter = {
+        PGIE_CLASS_ID_VEHICLE: 0,
+        PGIE_CLASS_ID_PERSON: 0,
+        PGIE_CLASS_ID_BICYCLE: 0,
+        PGIE_CLASS_ID_ROADSIGN: 0
+    }
+    num_rects = 0
+    gst_buffer = info.get_buffer()
+    if not gst_buffer:
+        print("Unable to get GstBuffer ")
+        return
+
+    # Retrieve batch metadata from the gst_buffer
+    # Note that pyds.gst_buffer_get_nvds_batch_meta() expects the
+    # C address of gst_buffer as input, which is obtained with hash(gst_buffer)
+    batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    l_frame = batch_meta.frame_meta_list
+    while l_frame is not None:
+        try:
+            # Note that l_frame.data needs a cast to pyds.NvDsFrameMeta
+            # The casting is done by pyds.NvDsFrameMeta.cast()
+            # The casting also keeps ownership of the underlying memory
+            # in the C code, so the Python garbage collector will leave
+            # it alone.
+            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+        except StopIteration:
+            break
+
+        frame_number = frame_meta.frame_num
+        num_rects = frame_meta.num_obj_meta
+        l_obj = frame_meta.obj_meta_list
+        while l_obj is not None:
+            try:
+                # Casting l_obj.data to pyds.NvDsObjectMeta
+                obj_meta = pyds.NvDsObjectMeta.cast(l_obj.data)
+            except StopIteration:
+                break
+            ## vehicle type detection ##
+            # Filter detections by PGIE network and don't include RoadSign class
+            if (obj_meta.unique_component_id == PGIE_UNIQUE_ID
+                    and obj_meta.class_id != PGIE_CLASS_ID_ROADSIGN  # Exclude RoadSign
+                    and obj_meta.class_id != PGIE_CLASS_ID_BICYCLE  # Exclude Bicycle
+                    and obj_meta.class_id != PGIE_CLASS_ID_PERSON  # Exclude Person
+                    ):
+                # get secondary classifier data
+                l_classifier = obj_meta.classifier_meta_list
+                sgie_class = -1
+                if l_classifier is not None:  # and class_id==XXX #apply classifier for a specific class
+                    classifier_meta = pyds.glist_get_nvds_classifier_meta(
+                        l_classifier.data)
+                    l_label = classifier_meta.label_info_list
+                    label_info = pyds.glist_get_nvds_label_info(l_label.data)
+                    sgie_class = label_info.result_class_id
+                    print("sgie_class >>>", sgie_class)
+
+            obj_counter[obj_meta.class_id] += 1
+            try:
+                l_obj = l_obj.next
+            except StopIteration:
+                break
+
+        # Acquiring a display meta object. The memory ownership remains in
+        # the C code so downstream plugins can still access it. Otherwise
+        # the garbage collector will claim it when this probe function exits.
+        display_meta = pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+        display_meta.num_labels = 1
+        py_nvosd_text_params = display_meta.text_params[0]
+        # Setting display text to be shown on screen
+        # Note that the pyds module allocates a buffer for the string, and the
+        # memory will not be claimed by the garbage collector.
+        # Reading the display_text field here will return the C address of the
+        # allocated string. Use pyds.get_string() to get the string content.
+        py_nvosd_text_params.display_text = "Frame Number={} Number of Objects={} Vehicle_count={} Person_count={}".format(
+            frame_number, num_rects, obj_counter[PGIE_CLASS_ID_VEHICLE], obj_counter[PGIE_CLASS_ID_PERSON])
+
+        # Now set the offsets where the string should appear
+        py_nvosd_text_params.x_offset = 10
+        py_nvosd_text_params.y_offset = 12
+
+        # Font , font-color and font-size
+        py_nvosd_text_params.font_params.font_name = "Serif"
+        py_nvosd_text_params.font_params.font_size = 10
+        # set(red, green, blue, alpha); set to White
+        py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
+
+        # Text background color
+        py_nvosd_text_params.set_bg_clr = 1
+        # set(red, green, blue, alpha); set to Black
+        py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
+        # Using pyds.get_string() to get display_text as string
+        print(pyds.get_string(py_nvosd_text_params.display_text))
+        pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+        try:
+            l_frame = l_frame.next
+        except StopIteration:
+            break
+    return Gst.PadProbeReturn.OK
+
+
+def cb_newpad(decodebin, decoder_src_pad, data):
+    print("In cb_newpad\n")
+    caps = decoder_src_pad.get_current_caps()
+    gststruct = caps.get_structure(0)
+    gstname = gststruct.get_name()
+    source_bin = data
+    features = caps.get_features(0)
+
+    # Need to check if the pad created by the decodebin is for video and not
+    # audio.
+    print("gstname=", gstname)
+    if(gstname.find("video") != -1):
+        # Link the decodebin pad only if decodebin has picked nvidia
+        # decoder plugin nvdec_*. We do this by checking if the pad caps contain
+        # NVMM memory features.
+        print("features=", features)
+        if features.contains("memory:NVMM"):
+            # Get the source bin ghost pad
+            bin_ghost_pad = source_bin.get_static_pad("src")
+            if not bin_ghost_pad.set_target(decoder_src_pad):
+                sys.stderr.write(
+                    "Failed to link decoder src pad to source bin ghost pad\n")
+        else:
+            sys.stderr.write(
+                " Error: Decodebin did not pick nvidia decoder plugin.\n")
+
+
+def decodebin_child_added(child_proxy, Object, name, user_data):
+    print("Decodebin child added:", name, "\n")
+    if(name.find("decodebin") != -1):
+        Object.connect("child-added", decodebin_child_added, user_data)
+    if(is_aarch64() and name.find("nvv4l2decoder") != -1):
+        print("Seting bufapi_version\n")
+        Object.set_property("bufapi-version", True)
+
+
+def create_source_bin(index, uri):
+    print("Creating source bin")
+
+    # Create a source GstBin to abstract this bin's content from the rest of the
+    # pipeline
+    bin_name = "source-bin-%02d" % index
+    print(bin_name)
+    nbin = Gst.Bin.new(bin_name)
+    if not nbin:
+        sys.stderr.write(" Unable to create source bin \n")
+
+    # Source element for reading from the uri.
+    # We will use decodebin and let it figure out the container format of the
+    # stream and the codec and plug the appropriate demux and decode plugins.
+    uri_decode_bin = Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
+    if not uri_decode_bin:
+        sys.stderr.write(" Unable to create uri decode bin \n")
+    # We set the input uri to the source element
+    uri_decode_bin.set_property("uri", uri)
+    # Connect to the "pad-added" signal of the decodebin which generates a
+    # callback once a new pad for raw data has beed created by the decodebin
+    uri_decode_bin.connect("pad-added", cb_newpad, nbin)
+    uri_decode_bin.connect("child-added", decodebin_child_added, nbin)
+
+    # We need to create a ghost pad for the source bin which will act as a proxy
+    # for the video decoder src pad. The ghost pad will not have a target right
+    # now. Once the decode bin creates the video decoder and generates the
+    # cb_newpad callback, we will set the ghost pad target to the video decoder
+    # src pad.
+    Gst.Bin.add(nbin, uri_decode_bin)
+    bin_pad = nbin.add_pad(
+        Gst.GhostPad.new_no_target("src", Gst.PadDirection.SRC))
+    if not bin_pad:
+        sys.stderr.write(" Failed to add ghost pad in source bin \n")
+        return None
+    return nbin
+
+
+def main(args):
+    # Check input arguments
+    if len(args) < 2:
+        sys.stderr.write("usage: %s <uri1> [uri2] ... [uriN]\n" % args[0])
+        sys.exit(1)
+
+    for i in range(0, len(args)-1):
+        fps_streams["stream{0}".format(i)] = GETFPS(i)
+
+    number_sources = len(args)-1
+
+    # Standard GStreamer initialization
+    GObject.threads_init()
+    Gst.init(None)
+
+    # Create gstreamer elements
+    # Create Pipeline element that will form a connection of other elements
+    print("Creating Pipeline \n ")
+    pipeline = Gst.Pipeline()
+
+    if not pipeline:
+        sys.stderr.write(" Unable to create Pipeline \n")
+
+    # READ RTSP/MP4 STREAM
+    # Create nvstreammux instance to form batches from one or more sources.
+    streammux = Gst.ElementFactory.make("nvstreammux", "Stream-muxer")
+    if not streammux:
+        sys.stderr.write(" Unable to create NvStreamMux \n")
+    pipeline.add(streammux)
+    # NOTE: number_sources is important!!!
+    for i in range(number_sources):
+        print("Creating source_bin ", i, " \n ")
+        uri_name = args[i+1]
+        if uri_name.find("rtsp://") == 0:
+            is_live = True
+        source_bin = create_source_bin(i, uri_name)
+        if not source_bin:
+            sys.stderr.write("Unable to create source bin \n")
+        pipeline.add(source_bin)
+        padname = "sink_%u" % i
+        sinkpad = streammux.get_request_pad(padname)
+        if not sinkpad:
+            sys.stderr.write("Unable to create sink pad bin \n")
+        srcpad = source_bin.get_static_pad("src")
+        if not srcpad:
+            sys.stderr.write("Unable to create src pad bin \n")
+        srcpad.link(sinkpad)
+    print("Creating Pgie \n ")
+    # Use nvinfer to run inferencing on decoder's output,
+    # behaviour of inferencing is set through config file
+    pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
+    if not pgie:
+        sys.stderr.write(" Unable to create pgie \n")
+
+    tracker = Gst.ElementFactory.make("nvtracker", "tracker")
+    if not tracker:
+        sys.stderr.write(" Unable to create tracker \n")
+
+    sgie = Gst.ElementFactory.make("nvinfer", "secondary-nvinference-engine")
+    if not sgie:
+        sys.stderr.write(" Unable to make sgie \n")
+
+    tiler = Gst.ElementFactory.make("nvmultistreamtiler", "nvtiler")
+    if not tiler:
+        sys.stderr.write(" Unable to create tiler \n")
+    print("Creating nvvidconv \n ")
+
+    nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
+    if not nvvidconv:
+        sys.stderr.write(" Unable to create nvvidconv \n")
+
+    # Create OSD to draw on the converted RGBA buffer
+    nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
+    if not nvosd:
+        sys.stderr.write(" Unable to create nvosd \n")
+
+    nvosd.set_property('process-mode', OSD_PROCESS_MODE)
+    nvosd.set_property('display-text', OSD_DISPLAY_TEXT)
+
+    if(is_aarch64()):
+        print("Creating transform \n ")
+        transform = Gst.ElementFactory.make(
+            "nvegltransform", "nvegl-transform")
+        if not transform:
+            sys.stderr.write(" Unable to create transform \n")
+
+    print("Creating EGLSink \n")
+    sink = Gst.ElementFactory.make("nveglglessink", "nvvideo-renderer")
+    if not sink:
+        sys.stderr.write(" Unable to create egl sink \n")
+
+    if is_live:
+        print("Atleast one of the sources is live")
+        streammux.set_property('live-source', 1)
+
+    print("Playing file %s " %args[1])
+    source.set_property('location', args[1])
+    streammux.set_property('width', 1920)
+    streammux.set_property('height', 1080)
+    streammux.set_property('batch-size', 1)
+    streammux.set_property('batched-push-timeout', 4000000)
+
+    pgie.set_property('config-file-path', PGIE_CONFIG_FILE)
+    sgie.set_property('config-file-path', SGIE_CONFIG_FILE)
+    # Set properties of tracker
+    config = configparser.ConfigParser()
+    config.read(TRACKER_KLT_CONFIG)
+    config.sections()
+
+    for key in config['tracker']:
+        if key == 'tracker-width':
+            tracker_width = config.getint('tracker', key)
+            tracker.set_property('tracker-width', tracker_width)
+        if key == 'tracker-height':
+            tracker_height = config.getint('tracker', key)
+            tracker.set_property('tracker-height', tracker_height)
+        if key == 'gpu-id':
+            tracker_gpu_id = config.getint('tracker', key)
+            tracker.set_property('gpu_id', tracker_gpu_id)
+        if key == 'll-lib-file':
+            tracker_ll_lib_file = config.get('tracker', key)
+            tracker.set_property('ll-lib-file', tracker_ll_lib_file)
+        if key == 'll-config-file':
+            tracker_ll_config_file = config.get('tracker', key)
+            tracker.set_property('ll-config-file', tracker_ll_config_file)
+        if key == 'enable-batch-process':
+            tracker_enable_batch_process = config.getint('tracker', key)
+            tracker.set_property('enable_batch_process',
+                                 tracker_enable_batch_process)
+
+    print("Adding elements to Pipeline \n")
+    pipeline.add(pgie)
+    pipeline.add(tracker)
+    pipeline.add(sgie)
+    pipeline.add(nvvidconv)
+    pipeline.add(nvosd)
+    pipeline.add(sink)
+    if is_aarch64():
+        pipeline.add(transform)
+
+    # we link the elements together
+    # rstp stream -> nvinfer -> nvinfer2 -> nvvidconv -> nvosd -> video-renderer
+    print("Linking elements in the Pipeline \n")
+    streammux.link(pgie)
+    pgie.link(tracker)
+    tracker.link(sgie)
+    sgie.link(nvvidconv)
+    nvvidconv.link(nvosd)
+    if is_aarch64():
+        nvosd.link(transform)
+        transform.link(sink)
+    else:
+        nvosd.link(sink)
+    # create and event loop and feed gstreamer bus mesages to it
+    loop = GObject.MainLoop()
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect("message", bus_call, loop)
+    # Lets add probe to get informed of the meta data generated, we add probe to
+    # the sink pad of the osd element, since by that time, the buffer would have
+    # had got all the metadata.
+    osdsinkpad = nvosd.get_static_pad("sink")
+    if not osdsinkpad:
+        sys.stderr.write(" Unable to get sink pad of nvosd \n")
+    osdsinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
+
+    print("Starting pipeline \n")
+
+    # start play back and listed to events
+    pipeline.set_state(Gst.State.PLAYING)
+    try:
+        loop.run()
+    except:
+        pass
+
+    # cleanup
+    pipeline.set_state(Gst.State.NULL)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
